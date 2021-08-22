e48e52da8f90b63c3d5525c424395b38e2257e86
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2021-08-22 23:30:12 +0900

    add lb

--- terraform-aws-eks-sample/eks.tf
+++ terraform-aws-eks-sample/eks.tf
@@ -9,10 +9,12 @@ module "eks" {
 
   node_groups = {
     ng-1 = {
-      desired_capacity = 2
-      max_capacity     = 2
-      min_capacity     = 2
-      instance_types   = ["t3.small"]
+      desired_capacity        = 2
+      max_capacity            = 2
+      min_capacity            = 2
+      instance_types          = ["t3.small"]
+      launch_template_id      = aws_launch_template.eks_example.id
+      launch_template_version = aws_launch_template.eks_example.latest_version
     }
   }
 
@@ -25,4 +27,18 @@ data "aws_eks_cluster" "cluster" {
 
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
+}
+
+resource "aws_launch_template" "eks_example" {
+  network_interfaces {
+    security_groups = [
+      module.eks.cluster_primary_security_group.id,
+      aws_security_group.node_example.id
+    ]
+  }
+}
+
+resource "aws_autoscaling_attachment" "eks_example" {
+  autoscaling_group_name = module.eks.node_groups["ng-1"].resources[0].autoscaling_groups[0].name
+  alb_target_group_arn   = aws_lb_target_group.example.arn
 }
--- terraform-aws-eks-sample/sg.tf
+++ terraform-aws-eks-sample/sg.tf
@@ -19,3 +19,18 @@ resource "aws_security_group" "lb_example" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group" "node_example" {
+  name   = "node-example"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+
+    security_groups = [
+      aws_security_group.lb_example.id
+    ]
+  }
+}
