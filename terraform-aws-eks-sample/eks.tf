5be1032065eeda502ca2276e50d2a5de6dce2810
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2021-08-22 23:18:43 +0900

    add sg

--- terraform-aws-eks-sample/eks.tf
+++ terraform-aws-eks-sample/eks.tf
@@ -1,6 +1,6 @@
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  version = "15.0.0"
+  version         = "15.0.0"
   cluster_name    = local.cluster_name
   cluster_version = local.cluster_version
   subnets         = module.vpc.public_subnets
@@ -8,21 +8,21 @@ module "eks" {
   vpc_id = module.vpc.vpc_id
 
   node_groups = {
-      ng-1 = {
-          desired_capacity = 2
-          max_capacity = 2
-          min_capacity = 2
-          instance_types = ["t3.small"]
-      }
+    ng-1 = {
+      desired_capacity = 2
+      max_capacity     = 2
+      min_capacity     = 2
+      instance_types   = ["t3.small"]
+    }
   }
 
   write_kubeconfig = false
 }
 
 data "aws_eks_cluster" "cluster" {
-    name = module.eks.cluster_id
+  name = module.eks.cluster_id
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-    name = module.eks.cluster_id
+  name = module.eks.cluster_id
 }
